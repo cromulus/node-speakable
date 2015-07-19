532924777d39c7223d62bce8e6c2f938e31e24fc
Bill Cromie <bill@cromie.org>
2015-07-18 22:04:06 -0400

    patching speakable

--- example.js
+++ example.js
@@ -1,8 +1,10 @@
 var Speakable = require('./');
-var API_KEY = process.env.GKEY;
+
 
 // Setup google speech
-var speakable = new Speakable({key: API_KEY});
+var speakable = new Speakable({
+  key: 'KEY'
+});
 
 speakable.on('speechStart', function() {
   console.log('onSpeechStart');
@@ -24,8 +26,35 @@ speakable.on('error', function(err) {
 });
 
 speakable.on('speechResult', function(spokenWords) {
-  console.log('onSpeechResult:')
+  console.log('onSpeechResult:');
   console.log(spokenWords);
+  var querystring = require('querystring');
+  var http = require('http');
+
+  var data = querystring.stringify({
+    words: spokenWords,
+  });
+
+  var options = {
+    host: 'my.url',
+    port: 80,
+    path: '/words',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(data)
+    }
+  };
+
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      console.log("body: " + chunk);
+    });
+  });
+
+  req.write(data);
+  req.end();
 });
 
 speakable.recordVoice();
